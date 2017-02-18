97027f2a98c74270b2ee35806562a15be973cf8f
Alex Tsankov <antsankov@gmail.com>
2017-02-18 14:55:03 -0700

    proper pulling

--- reddit.py
+++ reddit.py
@@ -2,22 +2,32 @@ import praw
 import random
 import urllib
 import os, datetime
-
+import tinys3
+import argparse
 from subprocess import call
 
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--content') 
+    parser.add_argument('--style') 
+    args = parser.parse_args()
 
-    dir_path = datetime.datetime.now().strftime('af_%Y-%m-%d_%H-%M-%S')
+    dir_path = datetime.datetime.now().strftime('af/%Y-%m-%d_%H-%M-%S')
     content_path = dir_path + '/content'
     style_path = dir_path + '/style'
-    output_path = dir_path + '/output.png'
+    output_path = dir_path + '/output' + time + '.png' 
 
     dir = os.path.join(os.getcwd(), dir_path)
     os.makedirs(dir)
 
-    gather_from_subreddit('art', content_path, style_path)
-    neural_style(style_path, content_path, output_path)
+    urllib.urlretrieve(args.style, style_path)
+    urllib.urlretrieve(args.content, content_path)
+    
+    # gather_from_subreddit('artporn', content_path, style_path)
+    neural_style(style_path, content_path, output_path, "0")
+    # neural_style(style_path, content_path, output_path_preserve, "1")
+
+    upload_to_s3(output_path)
 
 
 def gather_from_subreddit(sub, content_path, style_path):
@@ -38,17 +48,25 @@ def gather_from_subreddit(sub, content_path, style_path):
     style = top[random.randint(5,9)].url 
     urllib.urlretrieve(style, style_path)
 
-def neural_style(style, content, output):
+def upload_to_s3(file_path):
+    conn = tinys3.Connection(
+        os.getenv('S3_ACCESS_KEY'),os.getenv('S3_SECRET_KEY'),tls=True
+    )
+    f = open(file_path,'rb')
+    conn.upload(file_path,f,'art-factory')
+    
+
+def neural_style(style, content, output, original_colors):
     call([
-        "th", "neural_styleneural_style.lua",
+        "th", "neural_style.lua",
         "-gpu", "0",
         "-style_image", style, 
         "-content_image", content, 
         "-output_image", output,
         "-style_weight", "1000",
-        "-image_size", "512",
-        "-save_iter", "0"
-        # "-original_colors", "1"
+        "-image_size", "1024",
+        "-save_iter", "0",
+        "-original_colors", original_colors
     ])
 
 if __name__ == "__main__":
